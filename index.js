777fe78f60aef4b676dea37454dde7fe93f5073f
rezzak ali <rezzak ali>
2022-09-20 13:00:26 +0530

    make it better

--- index.js
+++ index.js
@@ -34,6 +34,9 @@ app.use(express.static(path.join(__dirname, './client/build')));
 
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static('./client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  });
 }
 
 // error handler
